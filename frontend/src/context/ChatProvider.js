f42f5f0eb1cc4144978bcbb48e38e71d540f078b
nhanhieuhcmus <vosihieunhan@gmail.com>
2021-12-09 11:49:14 +0700

    Remove log

--- frontend/src/context/ChatProvider.js
+++ frontend/src/context/ChatProvider.js
@@ -2,29 +2,23 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
 const ChatContext = createContext();
-console.log(">> jumped into ChatProvider !!");
 const ChatProvider = ({ children }) => {
     const [selectedChat, setSelectedChat] = useState(null);
     const [user, setUser] = useState(null);
     const [notification, setNotification] = useState([]);
     const [chats, setChats] = useState([]);
-    const [test, setTest] = useState("Test value");
 
     const history = useHistory();
 
     useEffect(() => {
         let userInfo;
         const unlistener = history.listen((location) => {
-            console.log(">>check location changed in ChatProvider: ", location);
+            console.log(location);
             userInfo = JSON.parse(localStorage.getItem("userInfo"));
             setUser(userInfo);
         });
         if (!userInfo) history.push("/");
         return unlistener;
-        // console.log(">>history changed in ChatProvider", history);
-        // const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-        // console.log(">> check userInfo in ChatProvider: ", userInfo);
-        // setUser(userInfo);
     }, [history]);
 
     return (
@@ -38,7 +32,6 @@ const ChatProvider = ({ children }) => {
                 setNotification,
                 chats,
                 setChats,
-                test,
             }}
         >
             {children}
@@ -47,7 +40,6 @@ const ChatProvider = ({ children }) => {
 };
 
 const ChatState = () => {
-    console.log(">>Jumped into ChatState in ChatProvider");
     return useContext(ChatContext);
 };
 
